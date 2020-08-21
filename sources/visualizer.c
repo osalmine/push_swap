9c30ac190139a1526791a7a5c00c52af98b4aef2
Onni Salminen <o.j.salminen@gmail.com>
2020-08-21 23:33:26 +0300

    Fixed visualizer

--- sources/visualizer.c
+++ sources/visualizer.c
@@ -6,7 +6,7 @@
 /*   By: osalmine <osalmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 13:43:17 by osalmine          #+#    #+#             */
-/*   Updated: 2020/03/27 00:44:43 by osalmine         ###   ########.fr       */
+/*   Updated: 2020/08/21 23:32:53 by osalmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,10 @@ static int	find_biggest_nb(t_ps *stack)
 
 static void	clear_screen(void)
 {
-	const char *clear_screen_ansi[10];
+	char clear_screen_ansi[11];
 
-	clear_screen_ansi = "\e[1;1H\e[2J";
+	ft_strcpy(clear_screen_ansi, "\e[1;1H\e[2J");
+	// clear_screen_ansi = "\e[1;1H\e[2J";
 	write(1, clear_screen_ansi, 10);
 }
 
